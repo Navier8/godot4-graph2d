554e6b7c2e2c423475a48e4fd76c032927359824
Navier8 <13157325+Navier8@users.noreply.github.com>
2025-01-12 22:16:39 +0000

    Added series to example

--- examples/static_plot.gd
+++ examples/static_plot.gd
@@ -1,12 +1,27 @@
 extends Control
 
 @onready var graph: Graph2D = $Graph2D
-@export var points: Array = [Vector2(0, 0), Vector2(2, 3), Vector2(3, 6), Vector2(4, 4)]
+@export var points: Array = [Vector2(0, 0), Vector2(2, 3.35), Vector2(3, 6), Vector2(4, 4)]
+
+var seriesn:int = 200
+var seriesx:PackedFloat64Array
+var seriesy:PackedFloat64Array
 
 func _ready() -> void:
 	UpdateChart()
 
 func UpdateChart() -> void:
-	var plot: PlotItem = graph.add_plot_item(" ", Color.WHITE, 2)
+	var plot1: PlotItem = graph.add_plot_item("Series 1", Color.WHITE, 2)
 	for point in points:
-		plot.add_point(point)
+		plot1.add_point(point)
+
+	seriesx.resize(seriesn)
+	seriesy.resize(seriesn)
+	for i:int in range(seriesn):
+		var x:float = 10.0 * float(i)/(seriesn-1)
+		var y:float = 10.0*sin(x * PI)
+		seriesx[i] = x
+		seriesy[i] = y
+
+	var plot2: PlotItem = graph.add_plot_item("Series 2", Color.RED, 2)
+	plot2.add_series(seriesx , seriesy)
