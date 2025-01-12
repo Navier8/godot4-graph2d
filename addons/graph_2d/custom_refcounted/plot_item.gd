2b261b8ec0ca665c5e4c22146112336a7164c72e
Navier8 <13157325+Navier8@users.noreply.github.com>
2025-01-12 22:17:45 +0000

    Point display
    Added new params to control display of points as circles

--- addons/graph_2d/custom_refcounted/plot_item.gd
+++ addons/graph_2d/custom_refcounted/plot_item.gd
@@ -26,14 +26,51 @@ var thickness: float = 1.0:
 		if value > 0.0:
 			thickness = value
 			_curve.width = thickness
-		
+
+## Draw a line
+var draw_line:bool = true:
+	set(value):
+		draw_line = value
+		_curve.draw_line = value
+
+## Draw circle point markers
+var draw_points:bool = true:
+	set(value):
+		draw_points = value
+		_curve.draw_points = value
+
+## Point size
+var point_size:float = 4.0:
+	set(value):
+		if value > 0.0:
+			point_size = value
+			_curve.point_size = point_size
+
+## Point color
+var point_color:Color = Color.WEB_GREEN:
+	set(value):
+		point_color = value
+		_curve.point_color = value
+
+## Filled points
+var point_filled:bool = false:
+	set(value):
+		point_filled = value
+		_curve.point_filled = value
+
+var point_frequency:int = 5:
+	set(value):
+		point_frequency = value
+		_curve.point_frequency = value
+
+
 var _curve
 var _LineCurve = preload("res://addons/graph_2d/custom_nodes/plot_2d.gd")
 var _points: PackedVector2Array
 var _graph
 
 
-func _init(obj, l, c, w):
+func _init(obj, l, c, w, ):
 	_curve = _LineCurve.new()
 	_graph = obj
 	label = l
@@ -67,7 +104,30 @@ func remove_point(pt: Vector2):
 	_curve.queue_redraw()
 
 
-## Remove all points from plot
+## Add series to plot
+func add_series(seriesx:PackedFloat64Array, seriesy:PackedFloat64Array):
+	if seriesx.size() != seriesy.size():
+		printerr("X and Y array dimensions do not match")
+
+	for i:int in range(seriesx.size()):
+		var pt: Vector2 = Vector2(seriesx[i] , seriesy[i])
+		_points.append(pt)
+		var point = pt.clamp(Vector2(_graph.x_min, _graph.y_min), Vector2(_graph.x_max, _graph.y_max))
+		var pt_px: Vector2
+		pt_px.x = remap(point.x, _graph.x_min, _graph.x_max, 0, _graph.get_node("PlotArea").size.x)
+		pt_px.y = remap(point.y, _graph.y_min, _graph.y_max, _graph.get_node("PlotArea").size.y, 0)
+		_curve.points_px.append(pt_px)
+	_curve.queue_redraw()
+
+
+## Update plot
+func update(seriesx:PackedFloat64Array, seriesy:PackedFloat64Array):
+	_points.clear()
+	_curve.points_px.clear()
+	add_series(seriesx,seriesy)
+	_curve.queue_redraw()
+
+## Remove all points (a series) from plot
 func remove_all():
 	_points.clear()
 	_curve.points_px.clear()
